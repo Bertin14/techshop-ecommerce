059e33301372596ae380ee1b58a9f9377badab7e
Bertin Rukundo <bertinrukundo@gmail.com>
2026-06-17 11:34:24 +0200

    fix: simplify config.php for Railway and Docker compatibility

--- includes/config.php
+++ includes/config.php
@@ -1,11 +1,6 @@
 <?php
 if (!defined('BASE_URL')) {
-    if (isset($_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST'] === 'localhost') {
-        define('BASE_URL', '/techshop');
-        define('ASSETS_URL', '/techshop/assets');
-    } else {
-        define('BASE_URL', '');
-        define('ASSETS_URL', '/assets');
-    }
+    define('BASE_URL', '');
+    define('ASSETS_URL', '/assets');
 }
 ?>
